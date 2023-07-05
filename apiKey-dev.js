c5ec3a1abc1cf9779225d5d5de819502b6982f2a
07khan86 <07khan86@gmail.com>
2023-07-05 12:52:30 +0530

    add apiKey

---
+++ apiKey-dev.js
@@ -0,0 +1,9 @@
+// const apiKey ="1kBmNnh69v93i5tOpj7bqqaJxjD3HJEucxd7egi7H";// "paste your apiKey from croquet.io/keys";
+// const appId = "io.croquet.microverse.localdevdefault";// "type your own appId such as com.example.david.mymicroverse";
+const apiKey = '1BropclkanzApH2bzludhy7hHKLi7G27a5jNnFckA';
+const appId = 'com.mobileprogramming.low.kumar.microverse';
+// const apiKey = '1u57OK8qb6uy9dtEAIb6I9AJqP163zGJpHOsomchA';
+// const appId = 'com.mobileprogramming.umashankar.viswanathan.microverse';
+const autoSleep = false; // Background tabs will go to sleep after 60 seconds or else we can use autoSleep =false to stop  reload totally
+
+export default {apiKey, appId, autoSleep};
---
+++ apiKey.js
@@ -0,0 +1,7 @@
+// const apiKey = '1u57OK8qb6uy9dtEAIb6I9AJqP163zGJpHOsomchA';// "paste your apiKey from croquet.io/keys";
+// const appId = "metaverse-79769.web.app";// "type your own appId such as com.example.david.mymicroverse";
+const apiKey = '1BropclkanzApH2bzludhy7hHKLi7G27a5jNnFckA';
+const appId = 'com.mobileprogramming.low.kumar.microverse';
+const autoSleep = false; // Background tabs will go to sleep after 60 seconds or else we can use autoSleep =false to stop  reload totally
+
+export default {apiKey, appId, autoSleep};
